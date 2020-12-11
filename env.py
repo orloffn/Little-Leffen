d290a20ae85596e68560e4b9c20fa773c3ee0afe
orloffn <ameofoxil@gmail.com>
2020-12-10 17:06:21 -0800

    environment for training using training csv

--- env.py
+++ env.py
@@ -3,26 +3,36 @@ https://stackoverflow.com/questions/44469266/how-to-implement-custom-environment
 https://github.com/openai/gym/blob/master/gym/envs/toy_text/hotter_colder.py
 """
 from time import sleep
+from ast import literal_eval
 import numpy as np
 import gym
 from gym import spaces
 from gym.utils import seeding
 
+import pandas as pd
+
 from gamestate import GameState
 from game import Game
 
-class Env(gym.Env):
-    """Environment to interact with melee"""
+ACTION_SPACE = spaces.Tuple((spaces.MultiDiscrete( \
+                            [2 for _ in range(GameState.NUM_ACTIONS - 2)]),
+                            spaces.Box(low=np.array([0.0, 0.0]), 
+                                       high=np.array([1.0, 1.0]),
+                                       dtype=np.float32),
+                            spaces.Discrete(5)))
+OBSERVATION_SPACE = spaces.Discrete(GameState.NUM_OBSERVATIONS)
+
+TRAINING_DATA_FILE = 'training_data.csv'
+SIMULATED_ACTION_THRESH = .01
+
+
+class Live(gym.Env):
+    """Environment to interact with dolphin"""
     def __init__(self, gs):
         self.gamestate = gs
         k = GameState.NUM_ACTIONS - 2
-        buttons = spaces.MultiDiscrete([2 for _ in range(k)])
-        grey_stick = spaces.Box(low=np.array([0.0, 0.0]), 
-                                high=np.array([1.0, 1.0]),
-                                dtype=np.float32)
-        c_stick = spaces.Discrete(5)
-        self.action_space = spaces.Tuple((buttons, grey_stick, c_stick))
-        self.observation_space = spaces.Discrete(GameState.NUM_OBSERVATIONS)
+        self.action_space = ACTION_SPACE
+        self.observation_space = OBSERVATION_SPACE
         self.seed()
         self.reset()
 
@@ -92,14 +102,59 @@ class Env(gym.Env):
                 port, int(player.x), int(player.y), player.percent))
 
 
-if __name__ == '__main__':
+class Simulated(gym.Env):
+    """docstring for Simulated"""
+    def __init__(self, file, size=None):
+        print('Loading training csv...')
+        self.df = pd.read_csv(file, header=None, nrows=size)
+        print('Training data loaded')
+        self.row_iter = None
+        self.current_state = None
+        self.action_space = ACTION_SPACE
+        self.observation_space = OBSERVATION_SPACE
+        self.seed()
+        self.reset()
+        
+    def step(self, action):
+        true = literal_eval(self.current_state[GameState.NUM_OBSERVATIONS])
+        pred = list(action[0]) + list(action[1]) + [action[2]]
+        reward = 0
+        for i in range(len(pred)):
+            if abs(true[i] - pred[i]) < SIMULATED_ACTION_THRESH:
+                reward += 1
+        try:
+            self.current_state = next(self.row_iter)[1]
+            done = False
+        except StopIteration:
+            done = True
+        return self.current_state.tolist()[:-1], reward, done, {}
+
+    def reset(self):
+        self.row_iter = self.df.iterrows()
+        self.current_state = next(self.row_iter)[1]
+
+    def render(self, mode='human', **kwargs):
+        print(self.current_state.tolist())
+
+
+def test_live():
     game = Game(2)
     gs = GameState(game)
-    env = Env(gs)
-    env.reset()
+    env = Live(gs)
     for i in range(1000):
         env.render()
         action = env.action_space.sample()
-        obs, _, done, _ = env.step(action)
+        obs, reward, done, _ = env.step(action)
         if done: env.reset()
 
+
+def test_sim():
+    env = Simulated(TRAINING_DATA_FILE, 100)
+    for i in range(199):
+        env.render()
+        action = env.action_space.sample()
+        obs, reward, done, _ = env.step(action)
+        if done: env.reset()
+
+if __name__ == '__main__':
+    test_sim()
